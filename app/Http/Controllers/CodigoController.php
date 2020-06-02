2952b4be63a437cf5ebaca27492fae5da1148517
Daniel Jimenez <srdejo@gmail.com>
2020-06-02 11:21:43 -0500

    Validar codigo

--- app/Http/Controllers/CodigoController.php
+++ app/Http/Controllers/CodigoController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Codigo;
+use App\User;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
@@ -13,8 +14,8 @@ class CodigoController extends Controller
     public function GenerarCodigo($celular)
     {
         try {
-            $codigo = New Codigo();
-            $codigo->codigo = rand(1000,9999);
+            $codigo = new Codigo();
+            $codigo->codigo = rand(1000, 9999);
             $codigo->celular = $celular;
             $codigo->save();
 
@@ -28,16 +29,22 @@ class CodigoController extends Controller
     {
         try {
             DB::beginTransaction();
-            $codigo = Codigo::where('celular',$request->celular)->where('codigo',$request->codigo)->whereNull('fecha_hora_utilizacion')->first();
-            if(isset($codigo)){
-                $fecha_hora_actual = Carbon::now();                
+            $user = User::firstOrNew(['celular' => $request->celular]);
+
+            $codigo = Codigo::where('celular', $request->celular)->where('codigo', $request->codigo)->whereNull('fecha_hora_utilizacion')->first();
+            if (isset($codigo) && isset($user)) {
+                $fecha_hora_actual = Carbon::now();
                 $codigo->fecha_hora_utilizacion = $fecha_hora_actual->toDateTimeString();
                 $codigo->save();
+
+                $user->codigo_id = $codigo->id;
+                $user->save();
+
                 DB::commit();
                 return response()->json([
                     'message' => 'Código validado correctamente'
                 ], 500);
-            }else{
+            } else {
                 DB::rollback();
                 return response()->json([
                     'message' => 'El código no corresponde!'
