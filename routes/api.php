be7f88ef873463d2eb1f6746218571d4f08ec11b
Daniel Jimenez <srdejo@gmail.com>
2020-06-03 13:31:16 -0500

    login

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::post('login', 'AuthController@login');
+Route::post('login', 'AuthController@registrar');
 Route::post('pre_registro', 'AuthController@registrar');
 Route::post('validar_codigo', 'CodigoController@ValidarCodigo');
 Route::put('direccion', 'DireccionController@ActualizarDireccion')->middleware('auth:api');
