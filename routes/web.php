b21cc98f37fba6f143bcadacd3eccb0f13ee6a2a
Daniel <danieljimenez@tmtek.net>
2020-07-20 17:54:52 -0500

    admin

--- routes/web.php
+++ routes/web.php
@@ -1,3 +1,15 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
+
+Auth::routes(['register' => false],['login' => false]);
+
+Route::get('/', 'HomeController@index')->name('inicio');
+Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('/login', 'Auth\LoginController@showAdminLoginForm')->name('login');
+Route::get('/register', 'Auth\RegisterController@showAdminRegisterForm');
+
+Route::post('/login', 'Auth\LoginController@adminLogin');
+Route::post('/register', 'Auth\RegisterController@createAdmin');
