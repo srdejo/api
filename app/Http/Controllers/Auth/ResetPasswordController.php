42dfe5d8fa608633d99426881182ba46156a8d78
Daniel <danieljimenez@tmtek.net>
2020-07-20 22:28:47 -0500

    Recuperar contraseña

--- app/Http/Controllers/Auth/ResetPasswordController.php
+++ app/Http/Controllers/Auth/ResetPasswordController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\ResetsPasswords;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Password;
 
 class ResetPasswordController extends Controller
 {
@@ -27,4 +29,14 @@ class ResetPasswordController extends Controller
      * @var string
      */
     protected $redirectTo = RouteServiceProvider::HOME;
+
+    protected function guard()
+    {
+        return Auth::guard('admin');
+    }
+
+    public function broker()
+    {
+        return Password::broker('admins');
+    }
 }
