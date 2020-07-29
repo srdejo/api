0f30c847441b048f05792640e090bf0664bdbccf
Daniel <danieljimenez@tmtek.net>
2020-07-29 07:30:19 -0500

    dos guard

--- app/Http/Controllers/ProductoController.php
+++ app/Http/Controllers/ProductoController.php
@@ -16,6 +16,7 @@ class ProductoController extends Controller
     public function __construct()
     {
         $this->middleware('auth:admin');
+        $this->middleware('auth');
     }
 
     public function getAll()
