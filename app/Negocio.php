a0c9c50a0d629bbec3ddb1c6937bcd13ec84f62f
Daniel <danieljimenez@tmtek.net>
2020-07-29 12:05:10 -0500

    Negocio

--- app/Negocio.php
+++ app/Negocio.php
@@ -6,6 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Negocio extends Model
 {
+    protected $hidden = [
+        'created_at', 'updated_at'
+    ];
+
     public function productos()
     {
         return $this->hasMany('App\Producto');
