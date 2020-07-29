3d8b3bf750431b48de573824ed9f21dfa5707324
Daniel <danieljimenez@tmtek.net>
2020-07-29 11:48:44 -0500

    full irl

--- app/Producto.php
+++ app/Producto.php
@@ -8,13 +8,13 @@ use Illuminate\Support\Facades\Storage;
 class Producto extends Model
 {
     protected $hidden = [
-        'created_at', 'updated_at'
+        'created_at', 'updated_at','imagen'
     ];
     
     protected $appends = array('url_imagen');
 
     public function getUrlImagenAttribute()
     {
-        return Storage::url($this->imagen);
+        return Storage::url($this->imagen)->full();
     }
 }
